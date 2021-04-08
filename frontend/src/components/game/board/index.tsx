6d3726d9d5601cc2abcd84bc56a216364a890fa8
Joël Billard <joel@corteks.fr>
2021-04-08 18:03:41 +0200

    Front components

--- frontend/src/components/game/board/index.tsx
+++ frontend/src/components/game/board/index.tsx
@@ -1,4 +1,7 @@
-import { ReactElement } from 'react';
+import { ReactElement, useContext } from 'react';
+import { useSelector } from 'react-redux';
+import { AppContext } from '../../..';
+import { AppState } from '../../../redux/reducers';
 import BoardComponent from './component';
 import './index.less';
 
@@ -11,12 +14,34 @@ export enum Colour {
     Red
 }
 
-const Board = (): ReactElement => <BoardComponent canMove={{
-    [Colour.Black]: true,
-    [Colour.Blue]: true,
-    [Colour.Purple]: true,
-    [Colour.Yellow]: true,
-    [Colour.Green]: false,
-    [Colour.Red]: true,
-}} />;
+const Board = (): ReactElement => {
+    const { game } = useContext(AppContext);
+
+    const mapStateToObj = useSelector((state: AppState) => {
+        const { possiblePlaces, pawnPlace, path } = state.game;
+        return {
+            possiblePlaces,
+            pawnPlace,
+            path
+        };
+    });
+
+    const clickPlace = (place: string) => {
+        game.clickPlace(place);
+        //game.placePawn(place);
+    };
+
+    const doubleClickPlace = (place: string) => {
+        game.doubleClickPlace(place);
+    };
+
+    return <BoardComponent canMove={{
+        [Colour.Black]: true,
+        [Colour.Blue]: true,
+        [Colour.Purple]: true,
+        [Colour.Yellow]: true,
+        [Colour.Green]: false,
+        [Colour.Red]: true,
+    }} placesHighlighted={mapStateToObj.possiblePlaces} pawnPlace={mapStateToObj.pawnPlace} path={mapStateToObj.path} clickPlace={clickPlace} doubleClickPlace={doubleClickPlace} />;
+};
 export default Board;
