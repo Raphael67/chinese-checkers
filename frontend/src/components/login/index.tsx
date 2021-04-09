8821dde2f852c3d4eb662f775269b1b21a062df9
Joël Billard <joel@corteks.fr>
2021-04-09 20:45:33 +0200

    Login changes

--- frontend/src/components/login/index.tsx
+++ frontend/src/components/login/index.tsx
@@ -1,21 +1,37 @@
 import LoginComponent from 'components/login/component';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { register } from 'redux/actions/session.action';
 import pages from '../../pages';
+import Api from '../../services/api';
 import { Colour } from '../game/board';
 
 const Login = (): ReactElement => {
     const dispatch = useDispatch();
-    //const params = useParams<IGame>();
+    const gameParams = useParams<IGameParams>();
 
+    const [errorMessage] = useState<string>();
+    const [game, setGame] = useState<IGame>();
 
-    const [errorMessage] = useState<string | undefined>(undefined);
     const history = useHistory();
 
+    useEffect(() => {
+        (async () => {
+            try {
+                setGame(await Api.getGame({
+                    gameId: gameParams.gameId
+                }));
+            }
+            catch (err) {
+                console.error(err);
+            }
+        })();
+
+    }, [gameParams.gameId]);
+
     const registerDispatch = (params: IRegisterParams) => {
-        register(dispatch, params);
+        register(dispatch, gameParams, params);
     };
 
     const goToGame = () => {
@@ -23,13 +39,17 @@ const Login = (): ReactElement => {
     };
 
     return <LoginComponent players={[{
-        colour: Colour.Blue,
-        name: 'dfg',
-        status: 'idle'
-    }, {
+        id: 1,
+        createdAt: new Date('2020-01-01'),
         colour: Colour.Yellow,
-        name: 'sdfsd',
+        nickname: 'sdfsd',
         status: 'disconnected'
+    }, {
+        id: 2,
+        createdAt: new Date('2020-01-01'),
+        colour: Colour.Black,
+        nickname: 'gg',
+        status: 'idle'
     }]} login={registerDispatch} goToGame={goToGame} errorMessage={errorMessage} />;
 };
 
