99c74a1a40d60755bf0dfc323aab54e9bc021d01
Raphael <raphael@corteks.fr>
2021-04-09 22:15:43 +0200

    fix unit test

--- backend/src/play/board.controller.spec.ts
+++ backend/src/play/board.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameService } from '../game/game.service';
 import { BoardController } from './board.controller';
+import { MoveService } from './move.service';
 
 class GameServiceMock extends GameService { }
+class MoveServiceMock extends MoveService { }
+
 
 describe('BoardController', () => {
     let controller: BoardController;
     let gameService: GameService = new GameServiceMock();
+    let moveService: MoveService = new MoveServiceMock();
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -15,6 +19,10 @@ describe('BoardController', () => {
                 {
                     provide: GameService,
                     useValue: gameService
+                },
+                {
+                    provide: MoveService,
+                    useValue: moveService,
                 }
             ]
         }).compile();
