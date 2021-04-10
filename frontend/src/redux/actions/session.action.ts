fa125f2c079b9047c2c2bf88789f6c44875a0e0f
Joël Billard <joel@corteks.fr>
2021-04-10 17:20:48 +0200

    Update session Redux state

--- frontend/src/redux/actions/session.action.ts
+++ frontend/src/redux/actions/session.action.ts
@@ -1,20 +1,19 @@
+import { ColourMap } from 'core/board';
 import { Dispatch } from 'react';
 import { ISetPlayerAction, Type } from 'redux/actions/types';
 import Api from 'services/api';
-import { ColourMap } from '../../core/board';
 
 export const register = async (dispatch: Dispatch<ISetPlayerAction>, gameParams: IGameParams, params: IRegisterParams) => {
-    try {
-        await Api.register(gameParams, params);
-        dispatch({
-            payload: {
-                nickname: params.nickname,
-                colour: ColourMap[params.color]
-            },
-            type: Type.SET_PLAYER
-        });
-    }
-    catch (err) {
+    await Api.register(gameParams, params).catch((err) => {
         throw err;
-    }
+    });
+
+    dispatch({
+        payload: {
+            nickname: params.nickname,
+            colour: ColourMap[params.color]
+        },
+        type: Type.SET_PLAYER
+    });
+
 };
