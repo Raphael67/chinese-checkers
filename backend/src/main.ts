c55e3be4e8fd65bb97a9030cebb9fc7fae312727
Raphael <raphael@corteks.fr>
2021-04-17 23:31:46 +0200

    improve swagger and dto definitions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
         whitelist: true,
         forbidNonWhitelisted: true,
         transform: true,
+        transformOptions: {
+            enableImplicitConversion: true,
+        },
     }));
 
     const configSerice = app.get<ConfigService>(ConfigService);
