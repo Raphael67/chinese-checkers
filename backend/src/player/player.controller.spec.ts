e22530845d44ffdae0b839583694d4f4f634c962
Raphael <raphael@corteks.fr>
2021-04-21 20:56:49 +0200

    remov eunused variable

--- backend/src/player/player.controller.spec.ts
+++ backend/src/player/player.controller.spec.ts
@@ -4,7 +4,6 @@ import { PlayerService } from './player.service';
 
 describe('PlayerController', () => {
     let controller: PlayerController;
-    let playerService: PlayerService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
