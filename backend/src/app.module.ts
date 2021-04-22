12e78b020f2085c688eadf0b1a2edeb894f17e29
Raphael <raphael@corteks.fr>
2021-04-22 12:12:01 +0200

    use nestjs configuration module for database access

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigType } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -7,15 +7,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { BotModule } from './bot/bot.module';
+import { default as mongodbConfig } from './config/mongodb.configuration';
 import { EventsModule } from './events/events.module';
 import { GameModule } from './game/game.module';
 import { PlayerModule } from './player/player.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
-        MongooseModule.forRoot('mongodb://localhost:27017/chinese-checkers', {
-            useFindAndModify: false,
+        ConfigModule.forRoot({
+            load: [mongodbConfig],
+            isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+            useFactory: (config: ConfigType<typeof mongodbConfig>) => {
+                console.log(config);
+                return {
+                    uri: `mongodb://${config.host}:${config.port}/chinese-checkers`,
+                    useFindAndModify: false,
+                };
+            },
+            inject: [mongodbConfig.KEY],
         }),
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '..', '..', 'frontend', 'build'),
